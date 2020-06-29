abe3ce3e3f22c9f16ce779c17e4849efa8d5321d
Khanh <khanhtq.b8084@st.usth.edu.vn>
2020-06-29 16:49:32 +0700

    new method to save to db

--- models/message.js
+++ models/message.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const messageSchema = new Schema({
   time: {type: Date, required: true},
   message: {type: String, required: true},
-  sender: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+  sender: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+  chatId: {type: Schema.Types.ObjectId, ref: 'Chat', required: true}
 })
 
 module.exports = mongoose.model('Message', messageSchema)
